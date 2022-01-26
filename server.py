f344a7d8953b359c103737fc37fa552a8b298161
SIDDHIHATTE <siddhihatte6223@gmail.com>
2022-01-26 13:21:45 +0530

    flask project code

---
+++ server.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('/index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def thankyou():
+    if request.method == 'POST': #if we have post method
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something is wrong'
+
+def write_to_file(data):
+    with open('database.csv', newline='', mode = 'a') as database2:
+
+        email = data['email']
+
+        subject = data['subject']
+
+        message = data['message']
+
+        csv_writer = csv.writer(database2, delimiter=",", quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+        csv_writer.writerow([email,subject,message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_file(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'Did not save to Database'
+    else:
+        return 'Something is wrong'
+
